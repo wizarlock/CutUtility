3d8495c1f89812fb59f41cb7c36ef6d787270e2e
wizarlock <shamaro.rv@edu.spbstu.ru>
2021-03-24 03:01:10 +0300

    upd

--- src/main/java/cutter/Cutter.java
+++ src/main/java/cutter/Cutter.java
@@ -12,28 +12,34 @@ import java.util.Scanner;
 public class Cutter {
 
 
-    private static List<String> cutText(List<String> list, String range, boolean cKey, boolean wKey) throws IOException {
+    private static List<String> cutText(List<String> list, String range, boolean cKey, boolean wKey) {
         String[] values = range.split("-");
         List<String> newText = new ArrayList<>();
         StringBuilder newLine = new StringBuilder();
         if (cKey) {
             if (range.charAt(0) == '-') {
                 for (String line : list) {
-                    for (int i = 0; i <=  Integer.parseInt(values[0]); i++) newLine.append(line.charAt(i));
+                    for (int i = 0; i <= Integer.parseInt(range.replace("-", "")); i++) {
+                        newLine.append(line.charAt(i));
+                        if (i == line.length() - 1) break;
+                    }
+                    newText.add(newLine.toString());
+                    newLine.setLength(0);
+
+                }
+            } else if (range.charAt(range.length() - 1) == '-') {
+                for (String line : list) {
+                    for (int i = Integer.parseInt(values[0]); i <= line.length() - 1; i++)
+                        newLine.append(line.charAt(i));
                     newText.add(newLine.toString());
                     newLine.setLength(0);
                 }
-            }
-            if (range.charAt(range.length() - 1) == '-') {
+            } else {
                 for (String line : list) {
-                    for (int flag = Integer.parseInt(values[0]); flag <= line.length() - 1; flag++) newLine.append(line.charAt(flag));
-                    newText.add(newLine.toString());
-                    newLine.setLength(0);
-                }
-            }
-            if (values.length == 2) {
-                for (String line : list) {
-                    for (int flag = Integer.parseInt(values[0]); flag <= Integer.parseInt(values[1]); flag++) newLine.append(line.charAt(flag));
+                    for (int i = Integer.parseInt(values[0]); i <= Integer.parseInt(values[1]); i++) {
+                        newLine.append(line.charAt(i));
+                        if (i == line.length() - 1) break;
+                    }
                     newText.add(newLine.toString());
                     newLine.setLength(0);
                 }
@@ -43,23 +49,28 @@ public class Cutter {
             if (range.charAt(0) == '-') {
                 for (String line : list) {
                     String[] words = line.split(" ");
-                    for (int i = 0; i <=  Integer.parseInt(values[0]); i++) newLine.append(words[i]).append(" ");
+                    for (int i = 0; i <= Integer.parseInt(range.replace("-", "")); i++) {
+                        newLine.append(words[i]).append(" ");
+                        if (i == words.length - 1) break;
+                    }
                     newText.add(newLine.toString().trim());
                     newLine.setLength(0);
                 }
-            }
-            if (range.charAt(range.length() - 1) == '-') {
+            } else if (range.charAt(range.length() - 1) == '-') {
                 for (String line : list) {
                     String[] words = line.split(" ");
-                    for ( int flag = Integer.parseInt(values[0]); flag <= words.length - 1; flag++) newLine.append(words[flag]).append(" ");
+                    for (int i = Integer.parseInt(values[0]); i <= words.length - 1; i++)
+                        newLine.append(words[i]).append(" ");
                     newText.add(newLine.toString().trim());
                     newLine.setLength(0);
                 }
-            }
-            if (values.length == 2) {
+            } else {
                 for (String line : list) {
                     String[] words = line.split(" ");
-                    for (int flag = Integer.parseInt(values[0]); flag <= Integer.parseInt(values[1]); flag++) newLine.append(words[flag]).append(" ");
+                    for (int i = Integer.parseInt(values[0]); i <= Integer.parseInt(values[1]); i++) {
+                        newLine.append(words[i]).append(" ");
+                        if (i == words.length - 1) break;
+                    }
                     newText.add(newLine.toString().trim());
                     newLine.setLength(0);
                 }
@@ -67,10 +78,10 @@ public class Cutter {
         }
         return newText;
     }
+    public void deleteLastLine(String outputName) {
 
-
+    }
     public static void cut(String inputName, String outputName, String range, boolean cKey, boolean wKey) throws IOException {
-
         if (inputName == null) {
             List<String> listOfString = new ArrayList<>();
             Scanner in = new Scanner(System.in);
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,34 +1,70 @@
 import cutter.Cutter;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class CutUtilityTest {
-    private void assertFileContent(String name, String expectedContent) throws IOException {
-        List<String> content = Files.readAllLines(Paths.get(name));
-        List<String> lines = new ArrayList<>(content);
-        List<String> newContent = Files.readAllLines(Paths.get(expectedContent));
-        List<String> newLines = new ArrayList<>(newContent);
-        assertEquals(lines, newLines);
+    private boolean assertFileContent(String actual, String expected) throws IOException {
+        List<String> expectedLines = Files.readAllLines(Paths.get(expected));
+        List<String> actualLines = Files.readAllLines(Paths.get(actual));
+        if (expectedLines.size() != actualLines.size()) return false;
+        for (int i = 0; i <= expectedLines.size() - 1; i++) {
+            if (!actualLines.get(i).equals(expectedLines.get(i))) return false;
+        }
+        return true;
     }
 
     @Test
     void cut() throws IOException {
         Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "0-2", false, true);
-        assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected.txt");
+        assertTrue(
+        assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "-4", false, true);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected2.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "-6", false, true);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected3.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "-100", false, true);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "3-", false, true);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected4.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "2-7", false, true);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected6.txt"));
 
         Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "8-16", true, false);
-        assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected1.txt");
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected1.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "8-100", true, false);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected7.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "-10", true, false);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected8.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "-100", true, false);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt"));
+
+        Cutter.cut("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\input\\input.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt", "5-", true, false);
+        assertTrue(
+                assertFileContent("C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\output\\output.txt","C:\\Users\\Виталий\\IdeaProjects\\CutUtility\\expected\\expected9.txt"));
     }
     }
 
